c8d37014efe4ca48f30e04c9991a5a9a5a89ec48
shelepuginivan <shelepuginivanm@gmail.com>
2024-08-06 01:10:18 +0300

    test(string): add test for function `WriteString`

--- string_test.go
+++ string_test.go
@@ -31,6 +31,30 @@ func TestReadString(t *testing.T) {
 	})
 }
 
+func TestWriteString(t *testing.T) {
+	t.Run("should write a string", func(t *testing.T) {
+		path := filepath.Join(t.TempDir(), "file.txt")
+		expected := "some string content that will be written very soon"
+		err := fsutil.WriteString(path, expected)
+
+		assert.NoError(t, err)
+
+		data, _ := os.ReadFile(path)
+		actual := string(data)
+
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("should return error if file cannot be written", func(t *testing.T) {
+		path := filepath.Join(t.TempDir(), "readonly.txt")
+		os.WriteFile(path, []byte{}, 0400) // readonly permission.
+
+		err := fsutil.WriteString(path, "it doesn't even matter")
+
+		assert.Error(t, err)
+	})
+}
+
 func TestReadLines(t *testing.T) {
 	t.Run("should return content as a slice of string", func(t *testing.T) {
 		path := filepath.Join(t.TempDir(), "file.txt")
