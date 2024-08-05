041ff5a8af65272bce101959982d063cf26221ce
shelepuginivan <shelepuginivanm@gmail.com>
2024-08-06 00:28:54 +0300

    test(write_all): add test for function `WriteAll`

---
+++ write_all_test.go
@@ -0,0 +1,33 @@
+package fsutil_test
+
+import (
+	"bytes"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/shelepuginivan/fsutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWriteAll(t *testing.T) {
+	t.Run("should write file and create all parent directories", func(t *testing.T) {
+		expected := []byte("content")
+		path := filepath.Join(t.TempDir(), "1", "2", "3", "file")
+
+		err := fsutil.WriteAll(path, expected)
+		assert.NoError(t, err)
+
+		actual, err := os.ReadFile(path)
+		assert.NoError(t, err)
+		assert.True(t, bytes.Equal(expected, actual))
+	})
+
+	t.Run("should return error if file cannot be written", func(t *testing.T) {
+		path := filepath.Join(t.TempDir(), "1")
+		filePath := filepath.Join(path, "2", "file")
+		os.WriteFile(path, []byte("exists"), os.ModePerm)
+		err := fsutil.WriteAll(filePath, []byte{})
+		assert.Error(t, err)
+	})
+}
