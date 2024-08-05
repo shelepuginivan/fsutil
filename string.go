53bd5e221094f64310fab28633cf8deaf6c321af
shelepuginivan <shelepuginivanm@gmail.com>
2024-08-06 01:48:38 +0300

    feat(string): add methods to write lines to file

--- string.go
+++ string.go
@@ -45,3 +45,38 @@ func ReadLines(path string) (lines []string, err error) {
 
 	return lines, nil
 }
+
+// WriteLines writes a slice of string as lines to a file.
+//
+// WriteLines uses platform-specific linebreak character (LF on Unix, CRLF of Windows).
+// Use [WriteLinesWithNewlineChar] if specific newline character is required.
+//
+// Permission perm is optional and defaults to [os.ModePerm].
+func WriteLines(path string, content []string, perm ...fs.FileMode) error {
+	return WriteLinesWithNewlineChar(path, content, NewLine, perm...)
+}
+
+// WriteLinesWithNewlineChar is like [WriteLines], but allows to set newline character used.
+func WriteLinesWithNewlineChar(path string, content []string, newline string, perm ...fs.FileMode) error {
+	// Append the default permission.
+	perm = append(perm, os.ModePerm)
+
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, perm[0])
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+
+	for i, line := range content {
+		writer.WriteString(line)
+
+		// Write a newline character if this is not the last line.
+		if i+1 < len(content) {
+			writer.WriteString(NewLine)
+		}
+	}
+
+	return writer.Flush()
+}
