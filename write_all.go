3ac7a6d517f81fd24e4f063d23bc672a59cd21d6
shelepuginivan <shelepuginivanm@gmail.com>
2024-08-06 04:23:30 +0300

    feat(write_all): add function `CreateAll`

--- write_all.go
+++ write_all.go
@@ -6,6 +6,18 @@ import (
 	"path/filepath"
 )
 
+// CreateAll creates file and all its parent directories.
+// If the file exists, it is truncated.
+func CreateAll(file string) (*os.File, error) {
+	dir := filepath.Dir(file)
+
+	if err := os.MkdirAll(dir, os.ModePerm|os.ModeDir); err != nil {
+		return nil, err
+	}
+
+	return os.Create(file)
+}
+
 // WriteAll writes data to path, creating all necessary parent directories.
 // If parent directories already exist, [WriteAll] does nothing to them.
 // If file already exists, it is overwritten with the given data.
