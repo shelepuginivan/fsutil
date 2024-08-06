22277cfe525bf93aae01d259c73f0d0bdcf88a48
shelepuginivan <shelepuginivanm@gmail.com>
2024-08-06 04:23:56 +0300

    test(write_all): add test for `CreateAll`

--- write_all_test.go
+++ write_all_test.go
@@ -10,6 +10,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestCreateAll(t *testing.T) {
+	t.Run("should create new file and parent directories", func(t *testing.T) {
+		path := filepath.Join(t.TempDir(), "1", "2", "3", "file")
+		file, err := fsutil.CreateAll(path)
+		assert.NoError(t, err)
+		file.Close()
+	})
+
+	t.Run("should return error if file cannot be created", func(t *testing.T) {
+		path := filepath.Join(t.TempDir(), "1")
+		filePath := filepath.Join(path, "2", "file")
+		os.WriteFile(path, []byte("exists"), os.ModePerm)
+		_, err := fsutil.CreateAll(filePath)
+		assert.Error(t, err)
+	})
+}
+
 func TestWriteAll(t *testing.T) {
 	t.Run("should write file and create all parent directories", func(t *testing.T) {
 		expected := []byte("content")
